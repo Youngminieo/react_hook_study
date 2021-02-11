6c6a7df762400cb2ea3ce6cc9f0a89d9a816df67
WI Young Min <youngminieo1005@gmail.com>
2021-02-11 21:27:14 +0900

    React useTitle

--- useTab.js
+++ useTab.js
@@ -9,4 +9,19 @@ const useTabs = (initialTab, allTabs) => {
     };
 };
 
-export default useTabs;
+const App = () => {
+    const { currentItem, changeItem } = useTabs(0, content);
+
+    return (
+        <div className="App">
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
+        </div>
+    );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
